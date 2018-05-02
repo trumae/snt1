02d505a489cb7015b7d1ef699101e383563bdebf
Vinicius Valente Maciel <vvmaciel@outlook.com>
2018-05-02 01:12:04 -0300

    reeng

--- client/main.cpp
+++ client/main.cpp
@@ -5,7 +5,7 @@
 #include <boost/asio.hpp>
 #include <boost/filesystem.hpp>
 
-#define DEBUG true
+#define DEBUG false
 
 using namespace std;
 using boost::asio::ip::tcp;
@@ -15,54 +15,82 @@ enum { max_length = 1024 };
 const string server = "localhost";
 const string port = "1234";
 
+void putText(string id, string filename)
+{
+    ifstream input(filename, std::ios::binary);
+
+    if (!input.is_open()) {
+        cout << "Failed to open " << filename << endl;
+        exit(1);
+    }
+
+    boost::asio::io_service io_service;
+    tcp::socket s(io_service);
+    tcp::resolver resolver(io_service);
+    boost::asio::connect(s, resolver.resolve({server, port}));
+
+    stringstream ss;
+    ss << id << " " << boost::filesystem::file_size(filename) << endl;
+
+    // send header
+    string header = ss.str();
+    boost::asio::write(s, boost::asio::buffer(header, header.length()));
+
+    // send content
+    char a;
+    string line;
+    while(input.read(&a, 1)) {
+        line += a;
+        if (a == '\n') {
+            size_t line_length = line.length();
+            size_t write_lenght = boost::asio::write(s, boost::asio::buffer(line, line_length));
+            if (DEBUG) cout << write_lenght << "|" << line ;
+            line = "";
+        }
+    }
+
+    cout <<  "Waiting ack" << endl;
+    char reply;
+    boost::asio::read(s, boost::asio::buffer(&reply, 1));
+    cout << "OK - Success" << endl;
+
+    exit(0);
+}
+
+void getText() {
+    cout << "getText" << endl;
+    exit(0);
+}
+
+void usage() {
+    cout << "Usage: client [put|get] <option>" << endl;
+    cout << " commands:" << endl;
+    cout << "   put <id> <path to text file>     put text to server" << endl;
+    cout << "   get                              get text from server" << endl;
+    exit(1);
+}
+
 int main(int argc, char* argv[]){
     try{
-        if (argc != 3) {
-            std::cerr << "Usage: client <id> <path to file>\n";
-            return 1;
-        }
-
-        string id = argv[1];
-        string filename = argv[2];
-
-        // open file for reading
-        ifstream input(filename, std::ios::binary);
-
-        if (!input.is_open()) {
-            cout << "Failed to open " << filename << endl;
-            return 1;
-        }
-
-        boost::asio::io_service io_service;
-        tcp::socket s(io_service);
-        tcp::resolver resolver(io_service);
-        boost::asio::connect(s, resolver.resolve({server, port}));
-
-        stringstream ss;
-        ss << id << " " << boost::filesystem::file_size(filename) << endl;
-
-        // send header
-        string header = ss.str();
-        boost::asio::write(s, boost::asio::buffer(header, header.length()));
-
-        // send content
-        char a;
-        string line;
-        while(input.read(&a, 1)) {
-            line += a;
-            if (a == '\n') {
-                size_t line_length = line.length();
-                size_t write_lenght = boost::asio::write(s, boost::asio::buffer(line, line_length));
-                if (DEBUG) cout << write_lenght << "|" << line ;
-                line = "";
+        if (argc == 2) {
+            string cmd = argv[1];
+            if (cmd == "get") {
+                getText();
             }
         }
 
-        cout <<  "Waiting ack" << endl;
-        char reply;
-        boost::asio::read(s, boost::asio::buffer(&reply, 1));
-        cout << "OK" << endl;
+        if (argc == 4) {
+            string cmd = argv[1];
+            if (cmd == "put") {
+                string id = argv[2];
+                string filename = argv[3];
 
+                // open file for reading
+                putText(id, filename);
+            }
+        }
+
+        usage();
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
